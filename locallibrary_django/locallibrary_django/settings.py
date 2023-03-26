2ccbafade620808cda1788042749156796ddd8a8
ccozens <ccozens@gmail.com>
2023-03-26 15:41:43 +0100

    added authentication and password reset sections

--- locallibrary_django/locallibrary_django/settings.py
+++ locallibrary_django/locallibrary_django/settings.py
@@ -125,3 +125,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = "/"
+
+# log code in sent emails to console
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
